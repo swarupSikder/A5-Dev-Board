f1d11e99656f3fef117f9b64f31c07d35520eb38
swarupSikder <swarupfx20@gmail.com>
2025-03-01 00:36:07 +0600

    complete button task added

--- scripts/main_board.js
+++ scripts/main_board.js
@@ -1,14 +1,14 @@
 let totalCompletedTaskCount = parseInt(document.getElementById('total-completed-task-count').innerText);
 let assignedTaskCount = parseInt(document.getElementById('assigned-task-count').innerText);
 
-completeTask('c1');
-completeTask('c2');
-completeTask('c3');
-completeTask('c4');
-completeTask('c5');
-completeTask('c6');
+completeTask('c1', 'item-1');
+completeTask('c2', 'item-2');
+completeTask('c3', 'item-3');
+completeTask('c4', 'item-4');
+completeTask('c5', 'item-5');
+completeTask('c6', 'item-6');
 
-function completeTask(id) {
+function completeTask(id, itemID) {
     document.getElementById(id).addEventListener('click', function (event) {
         event.preventDefault();
         assignedTaskCount--;
@@ -17,7 +17,17 @@ function completeTask(id) {
         document.getElementById('total-completed-task-count').innerText = totalCompletedTaskCount;
 
         //parent
-        const parent = document.getElementById();
+        const parent = document.getElementById('activity-container');
+        //child
+        const child = document.createElement('div');
+        child.innerHTML =
+        `
+            <p class="bg-base-color p-3 my-3 rounded-md text-sm">
+                You have Complete The Task Add Dark Mode at 12:48:15 PM
+            </p>
+        `;
+        //parent << child
+        parent.appendChild(child);
         this.disabled = true;
     });
 }
