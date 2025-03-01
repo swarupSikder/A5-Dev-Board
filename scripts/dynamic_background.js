66b993732c6c49f9d15ba08c84d835a1b6ff2004
swarupSikder <swarupfx20@gmail.com>
2025-03-01 14:48:40 +0600

    dynamic-background functionality added

---
+++ scripts/dynamic_background.js
@@ -0,0 +1,14 @@
+let backgroundList = ['#d9ead3', '#fce5cd', '#CCC', '#c9bbc8', '#aac9ce', '#F4F7FF'];
+let i = 0;
+
+document.getElementById('dyn-bg').addEventListener('click', function(){
+    //main task (bg change)
+    console.log(backgroundList[i]);
+    document.body.style.backgroundColor = backgroundList[i];
+
+    //increment
+    i++;
+    if(i===backgroundList.length){
+        i=0;
+    }
+});
